61e400a50db451134057ce84eab24331aec26750
Thebettercure <122114240+Thebettercure@users.noreply.github.com>
2026-06-11 16:31:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const video = document.getElementById("video");
+const startBtn = document.getElementById("startBtn");
+const toggleBtn = document.getElementById("toggleBtn");
+
+let inverted = true;
+
+startBtn.addEventListener("click", async () => {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: false
+        });
+
+        video.srcObject = stream;
+    } catch (err) {
+        alert("Could not access camera: " + err.message);
+    }
+});
+
+toggleBtn.addEventListener("click", () => {
+    inverted = !inverted;
+    video.style.filter = inverted
+        ? "invert(100%)"
+        : "invert(0%)";
+});
